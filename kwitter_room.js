a3a44b0f4d6befd1b9c2c8d91155bab4125aa797
Nithya-Kannan <77827910+Nithya-Kannan@users.noreply.github.com>
2024-06-03 16:48:04 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,12 +12,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+user_name=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome "+user_name+"!";
 
-  
-  
   function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
          Room_names = childKey;
-        
+         console.log("room name -"+Room_names);
+         row="<div class='room_name' id=" + Room_names+ " onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div> <hr>";
+         document.getElementById("output").innerHTML+=row;
         });});}
   getData();
 
@@ -29,6 +31,12 @@ firebase.initializeApp(firebaseConfig);
     });
     localStorage.setItem("room_name",room_name);
     window.location="kwitter_page.html"
+  }
+
+  function redirectToRoomName(name){
+    console.log(name);
+    localStorage.setItem("room_name",name);
+    window.location="kwitter_page.html";
   }
   
   
